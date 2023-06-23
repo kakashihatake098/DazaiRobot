0f9444e67b09d42489c4619ce5a58057c4ec5af0
adi6804 <120642069+adi6804@users.noreply.github.com>
2023-06-23 23:14:17 +0530

    Update muting.py

--- DazaiRobot/modules/muting.py
+++ DazaiRobot/modules/muting.py
@@ -6,20 +6,20 @@ from telegram.error import BadRequest
 from telegram.ext import CallbackContext, CommandHandler
 from telegram.utils.helpers import mention_html
 
-from FallenRobot import LOGGER, TIGERS, dispatcher
-from FallenRobot.modules.helper_funcs.chat_status import (
+from DazaiRobot import LOGGER, TIGERS, dispatcher
+from DazaiRobot.modules.helper_funcs.chat_status import (
     bot_admin,
     can_restrict,
     connection_status,
     is_user_admin,
     user_admin,
 )
-from FallenRobot.modules.helper_funcs.extraction import (
+from DazaiRobot.modules.helper_funcs.extraction import (
     extract_user,
     extract_user_and_text,
 )
-from FallenRobot.modules.helper_funcs.string_handling import extract_time
-from FallenRobot.modules.log_channel import loggable
+from DazaiRobot.modules.helper_funcs.string_handling import extract_time
+from DazaiRobot.modules.log_channel import loggable
 
 
 def check_user(user_id: int, bot: Bot, chat: Chat) -> Optional[str]:
