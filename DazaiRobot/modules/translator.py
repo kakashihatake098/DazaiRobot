915b4d2572a1cd47e9677b732d55c1c2ecda8a61
adi6804 <120642069+adi6804@users.noreply.github.com>
2023-06-23 23:22:22 +0530

    Update translator.py

--- DazaiRobot/modules/translator.py
+++ DazaiRobot/modules/translator.py
@@ -2,8 +2,8 @@ from gpytranslate import SyncTranslator
 from telegram import ParseMode, Update
 from telegram.ext import CallbackContext
 
-from FallenRobot import dispatcher
-from FallenRobot.modules.disable import DisableAbleCommandHandler
+from DazaiRobot import dispatcher
+from DazaiRobot.modules.disable import DisableAbleCommandHandler
 
 trans = SyncTranslator()
 
