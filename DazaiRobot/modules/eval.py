1ca8d6eeabc552719ae900fdca702f76ca1019d0
adi6804 <120642069+adi6804@users.noreply.github.com>
2023-06-23 23:04:55 +0530

    Update eval.py

--- DazaiRobot/modules/eval.py
+++ DazaiRobot/modules/eval.py
@@ -9,8 +9,8 @@ from contextlib import redirect_stdout
 from telegram import ParseMode, Update
 from telegram.ext import CallbackContext, CommandHandler
 
-from FallenRobot import LOGGER, dispatcher
-from FallenRobot.modules.helper_funcs.chat_status import dev_plus
+from DazaiRobot import LOGGER, dispatcher
+from DazaiRobot.modules.helper_funcs.chat_status import dev_plus
 
 namespaces = {}
 
