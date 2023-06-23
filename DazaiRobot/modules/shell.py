4a41545456676ae0e72e0670b9e9d7037faabb92
adi6804 <120642069+adi6804@users.noreply.github.com>
2023-06-23 23:19:08 +0530

    Update shell.py

--- DazaiRobot/modules/shell.py
+++ DazaiRobot/modules/shell.py
@@ -3,8 +3,8 @@ import subprocess
 from telegram import ParseMode, Update
 from telegram.ext import CallbackContext, CommandHandler
 
-from FallenRobot import LOGGER, dispatcher
-from FallenRobot.modules.helper_funcs.chat_status import dev_plus
+from DazaiRobot import LOGGER, dispatcher
+from DazaiRobot.modules.helper_funcs.chat_status import dev_plus
 
 
 @dev_plus
