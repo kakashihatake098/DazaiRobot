0893c0121cc428ea65755ab0809af204c2cec4d3
adi6804 <120642069+adi6804@users.noreply.github.com>
2023-06-23 23:10:44 +0530

    Update locks.py

--- DazaiRobot/modules/locks.py
+++ DazaiRobot/modules/locks.py
@@ -6,20 +6,20 @@ from telegram.error import BadRequest
 from telegram.ext import CommandHandler, Filters, MessageHandler
 from telegram.utils.helpers import mention_html
 
-import FallenRobot.modules.sql.locks_sql as sql
-from FallenRobot import DRAGONS, LOGGER, dispatcher
-from FallenRobot.modules.connection import connected
-from FallenRobot.modules.disable import DisableAbleCommandHandler
-from FallenRobot.modules.helper_funcs.alternate import send_message, typing_action
-from FallenRobot.modules.helper_funcs.chat_status import (
+import DazaiRobot.modules.sql.locks_sql as sql
+from DazaiRobot import DRAGONS, LOGGER, dispatcher
+from DazaiRobot.modules.connection import connected
+from DazaiRobot.modules.disable import DisableAbleCommandHandler
+from DazaiRobot.modules.helper_funcs.alternate import send_message, typing_action
+from DazaiRobot.modules.helper_funcs.chat_status import (
     can_delete,
     is_bot_admin,
     is_user_admin,
     user_admin,
     user_not_admin,
 )
-from FallenRobot.modules.log_channel import loggable
-from FallenRobot.modules.sql.approve_sql import is_approved
+from DazaiRobot.modules.log_channel import loggable
+from DazaiRobot.modules.sql.approve_sql import is_approved
 
 
 def al_detect(unistr):
