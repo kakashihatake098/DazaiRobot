ab04f52314bbc105d81ef762e92596dbe2fe7742
adi6804 <120642069+adi6804@users.noreply.github.com>
2023-06-23 22:57:42 +0530

    Update country.py

--- DazaiRobot/modules/country.py
+++ DazaiRobot/modules/country.py
@@ -1,8 +1,8 @@
 import flag
 from countryinfo import CountryInfo
 
-from FallenRobot import BOT_NAME, telethn
-from FallenRobot.events import register
+from DazaiRobot import BOT_NAME, telethn
+from DazaiRobot.events import register
 
 
 @register(pattern="^/country (.*)")
