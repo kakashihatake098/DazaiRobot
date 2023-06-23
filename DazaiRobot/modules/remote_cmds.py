4e42ac4d1ba1a397debbfd34986a8d734388344d
adi6804 <120642069+adi6804@users.noreply.github.com>
2023-06-23 23:17:26 +0530

    Update remote_cmds.py

--- DazaiRobot/modules/remote_cmds.py
+++ DazaiRobot/modules/remote_cmds.py
@@ -2,15 +2,15 @@ from telegram import ChatPermissions, Update
 from telegram.error import BadRequest
 from telegram.ext import CallbackContext, CommandHandler
 
-from FallenRobot import dispatcher
-from FallenRobot.modules.helper_funcs.chat_status import (
+from DazaiRobot import dispatcher
+from DazaiRobot.modules.helper_funcs.chat_status import (
     bot_admin,
     is_bot_admin,
     is_user_ban_protected,
     is_user_in_chat,
 )
-from FallenRobot.modules.helper_funcs.extraction import extract_user_and_text
-from FallenRobot.modules.helper_funcs.filters import CustomFilters
+from DazaiRobot.modules.helper_funcs.extraction import extract_user_and_text
+from DazaiRobot.modules.helper_funcs.filters import CustomFilters
 
 RBAN_ERRORS = {
     "User is an administrator of the chat",
