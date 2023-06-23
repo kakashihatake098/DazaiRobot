eb101589e76af8d992334ca19e46e7c85cea79d6
adi6804 <120642069+adi6804@users.noreply.github.com>
2023-06-23 22:18:22 +0530

    Update admins.py

--- DazaiRobot/utils/admins.py
+++ DazaiRobot/utils/admins.py
@@ -3,7 +3,7 @@ from typing import Callable
 from pyrogram.enums import ChatMemberStatus
 from pyrogram.types import Message
 
-from FallenRobot import DEV_USERS, DRAGONS, pbot
+from DazaiRobot import DEV_USERS, DRAGONS, pbot
 
 
 def can_change_info(func: Callable) -> Callable:
