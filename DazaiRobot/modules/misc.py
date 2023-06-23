0a8fb66f4c4013bb6c6a8670d6f537ac256793ae
adi6804 <120642069+adi6804@users.noreply.github.com>
2023-06-23 23:12:45 +0530

    Update misc.py

--- DazaiRobot/modules/misc.py
+++ DazaiRobot/modules/misc.py
@@ -1,9 +1,9 @@
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup, ParseMode, Update
 from telegram.ext import CallbackContext, CommandHandler, Filters
 
-from FallenRobot import dispatcher
-from FallenRobot.modules.disable import DisableAbleCommandHandler
-from FallenRobot.modules.helper_funcs.chat_status import user_admin
+from DazaiRobot import dispatcher
+from DazaiRobot.modules.disable import DisableAbleCommandHandler
+from DazaiRobot.modules.helper_funcs.chat_status import user_admin
 
 MARKDOWN_HELP = f"""
 Markdown is a very powerful formatting tool supported by telegram. {dispatcher.bot.first_name} has some enhancements, to make sure that \
