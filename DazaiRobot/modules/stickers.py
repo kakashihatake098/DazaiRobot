8c5adda85db8b823c7fe6752de9709f54c0a43f3
adi6804 <120642069+adi6804@users.noreply.github.com>
2023-06-23 23:20:41 +0530

    Update stickers.py

--- DazaiRobot/modules/stickers.py
+++ DazaiRobot/modules/stickers.py
@@ -16,8 +16,8 @@ from telegram import (
 from telegram.ext import CallbackContext
 from telegram.utils.helpers import mention_html
 
-from FallenRobot import dispatcher
-from FallenRobot.modules.disable import DisableAbleCommandHandler
+from DazaiRobot import dispatcher
+from DazaiRobot.modules.disable import DisableAbleCommandHandler
 
 combot_stickers_url = "https://combot.org/telegram/stickers?q="
 
