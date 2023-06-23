d6082b602fb56c9eee97b383e574b10314596b37
adi6804 <120642069+adi6804@users.noreply.github.com>
2023-06-23 22:10:22 +0530

    Update log_channel_sql.py

--- DazaiRobot/modules/sql/log_channel_sql.py
+++ DazaiRobot/modules/sql/log_channel_sql.py
@@ -2,7 +2,7 @@ import threading
 
 from sqlalchemy import Column, String, distinct, func
 
-from FallenRobot.modules.sql import BASE, SESSION
+from DazaiRobot.modules.sql import BASE, SESSION
 
 
 class GroupLogs(BASE):
