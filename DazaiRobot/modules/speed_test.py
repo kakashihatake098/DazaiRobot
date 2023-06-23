551f83865b7ca1a12ffc8217b33e0ffadfc08590
adi6804 <120642069+adi6804@users.noreply.github.com>
2023-06-23 23:20:18 +0530

    Update speed_test.py

--- DazaiRobot/modules/speed_test.py
+++ DazaiRobot/modules/speed_test.py
@@ -2,9 +2,9 @@ import speedtest
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup, ParseMode, Update
 from telegram.ext import CallbackContext, CallbackQueryHandler
 
-from FallenRobot import DEV_USERS, dispatcher
-from FallenRobot.modules.disable import DisableAbleCommandHandler
-from FallenRobot.modules.helper_funcs.chat_status import dev_plus
+from DazaiRobot import DEV_USERS, dispatcher
+from DazaiRobot.modules.disable import DisableAbleCommandHandler
+from DazaiRobot.modules.helper_funcs.chat_status import dev_plus
 
 
 def convert(speed):
