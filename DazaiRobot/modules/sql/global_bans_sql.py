03f704274ea7ebefa02f650e85d1a22303c615e5
adi6804 <120642069+adi6804@users.noreply.github.com>
2023-06-23 22:08:47 +0530

    Update global_bans_sql.py

--- DazaiRobot/modules/sql/global_bans_sql.py
+++ DazaiRobot/modules/sql/global_bans_sql.py
@@ -2,7 +2,7 @@ import threading
 
 from sqlalchemy import BigInteger, Boolean, Column, String, UnicodeText
 
-from FallenRobot.modules.sql import BASE, SESSION
+from DazaiRobot.modules.sql import BASE, SESSION
 
 
 class GloballyBannedUsers(BASE):
