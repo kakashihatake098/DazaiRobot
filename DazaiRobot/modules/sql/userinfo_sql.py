dbfc95e5996d5e0d9429f557970ef83d75415ac4
adi6804 <120642069+adi6804@users.noreply.github.com>
2023-06-23 22:13:41 +0530

    Update userinfo_sql.py

--- DazaiRobot/modules/sql/userinfo_sql.py
+++ DazaiRobot/modules/sql/userinfo_sql.py
@@ -2,7 +2,7 @@ import threading
 
 from sqlalchemy import BigInteger, Column, UnicodeText
 
-from FallenRobot.modules.sql import BASE, SESSION
+from DazaiRobot.modules.sql import BASE, SESSION
 
 
 class UserInfo(BASE):
