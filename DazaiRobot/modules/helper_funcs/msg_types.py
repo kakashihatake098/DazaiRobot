c18cb31ebeac42d6e741cc3377d0ea4d21507f5a
adi6804 <120642069+adi6804@users.noreply.github.com>
2023-06-23 21:52:26 +0530

    Update msg_types.py

--- DazaiRobot/modules/helper_funcs/msg_types.py
+++ DazaiRobot/modules/helper_funcs/msg_types.py
@@ -2,7 +2,7 @@ from enum import IntEnum, unique
 
 from telegram import Message
 
-from FallenRobot.modules.helper_funcs.string_handling import button_markdown_parser
+from DazaiRobot.modules.helper_funcs.string_handling import button_markdown_parser
 
 
 @unique
