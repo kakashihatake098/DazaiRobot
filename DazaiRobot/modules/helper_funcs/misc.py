234b8164dfd8c180c420ebd25fbeebd2a07d5946
adi6804 <120642069+adi6804@users.noreply.github.com>
2023-06-23 21:51:05 +0530

    Update misc.py

--- DazaiRobot/modules/helper_funcs/misc.py
+++ DazaiRobot/modules/helper_funcs/misc.py
@@ -13,7 +13,7 @@ from telegram import (
 )
 from telegram.error import TelegramError
 
-from FallenRobot import NO_LOAD
+from DazaiRobot import NO_LOAD
 
 
 class EqInlineKeyboardButton(InlineKeyboardButton):
@@ -90,7 +90,7 @@ def paginate_modules(page_n: int, module_dict: Dict, prefix, chat=None) -> List:
                 EqInlineKeyboardButton(
                     "◁", callback_data="{}_prev({})".format(prefix, modulo_page)
                 ),
-                EqInlineKeyboardButton("• ʜᴏᴍᴇ •", callback_data="fallen_back"),
+                EqInlineKeyboardButton("• ʜᴏᴍᴇ •", callback_data="dazai_back"),
                 EqInlineKeyboardButton(
                     "▷", callback_data="{}_next({})".format(prefix, modulo_page)
                 ),
@@ -98,7 +98,7 @@ def paginate_modules(page_n: int, module_dict: Dict, prefix, chat=None) -> List:
         ]
 
     else:
-        pairs += [[EqInlineKeyboardButton("• ʙᴀᴄᴋ •", callback_data="fallen_back")]]
+        pairs += [[EqInlineKeyboardButton("• ʙᴀᴄᴋ •", callback_data="dazai_back")]]
 
     return pairs
 
