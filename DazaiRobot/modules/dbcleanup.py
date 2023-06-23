4f4184c3e3fbe6044446ac10a8e7ed47eb6e6243
adi6804 <120642069+adi6804@users.noreply.github.com>
2023-06-23 22:59:43 +0530

    Update dbcleanup.py

--- DazaiRobot/modules/dbcleanup.py
+++ DazaiRobot/modules/dbcleanup.py
@@ -4,10 +4,10 @@ from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update
 from telegram.error import BadRequest, Unauthorized
 from telegram.ext import CallbackContext, CallbackQueryHandler, CommandHandler
 
-import FallenRobot.modules.sql.global_bans_sql as gban_sql
-import FallenRobot.modules.sql.users_sql as user_sql
-from FallenRobot import DEV_USERS, OWNER_ID, dispatcher
-from FallenRobot.modules.helper_funcs.chat_status import dev_plus
+import DazaiRobot.modules.sql.global_bans_sql as gban_sql
+import DazaiRobot.modules.sql.users_sql as user_sql
+from DazaiRobot import DEV_USERS, OWNER_ID, dispatcher
+from DazaiRobot.modules.helper_funcs.chat_status import dev_plus
 
 
 def get_invalid_chats(update: Update, context: CallbackContext, remove: bool = False):
