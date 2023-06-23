d07891998d7ef752ccdfc8af4ba49b70be769535
adi6804 <120642069+adi6804@users.noreply.github.com>
2023-06-23 22:17:01 +0530

    Update welcome_sql.py

--- DazaiRobot/modules/sql/welcome_sql.py
+++ DazaiRobot/modules/sql/welcome_sql.py
@@ -4,8 +4,8 @@ from typing import Union
 
 from sqlalchemy import BigInteger, Boolean, Column, Integer, String, UnicodeText
 
-from FallenRobot.modules.helper_funcs.msg_types import Types
-from FallenRobot.modules.sql import BASE, SESSION
+from DazaiRobot.modules.helper_funcs.msg_types import Types
+from DazaiRobot.modules.sql import BASE, SESSION
 
 DEFAULT_WELCOME = "Hey {first}, how are you?"
 DEFAULT_GOODBYE = "Nice knowing ya!"
