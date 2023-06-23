a5f92e4fe9fa5dd2135d4bdf6c9639fb1c0dc273
adi6804 <120642069+adi6804@users.noreply.github.com>
2023-06-23 22:20:06 +0530

    Update mongo.py

--- DazaiRobot/utils/mongo.py
+++ DazaiRobot/utils/mongo.py
@@ -2,7 +2,7 @@ from typing import Dict, Union
 
 from motor.motor_asyncio import AsyncIOMotorClient as MongoCli
 
-from FallenRobot import MONGO_DB_URI
+from DazaiRobot import MONGO_DB_URI
 
 mongo = MongoCli(MONGO_DB_URI)
 db = mongo.FallenRobot
