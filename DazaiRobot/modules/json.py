225b7b2ae766055b8b714dc3775e60e40fbd4426
adi6804 <120642069+adi6804@users.noreply.github.com>
2023-06-23 23:09:46 +0530

    Update json.py

--- DazaiRobot/modules/json.py
+++ DazaiRobot/modules/json.py
@@ -3,8 +3,8 @@ import io
 from telethon import types
 from telethon.tl import functions
 
-from FallenRobot import telethn as tbot
-from FallenRobot.events import register
+from DazaiRobot import telethn as tbot
+from DazaiRobot.events import register
 
 
 async def is_register_admin(chat, user):
