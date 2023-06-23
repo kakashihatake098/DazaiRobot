b337e67cde68f31e13e4e2945cb62fc757589a9c
adi6804 <120642069+adi6804@users.noreply.github.com>
2023-06-23 21:49:16 +0530

    handlers.py

--- DazaiRobot/modules/helper_funcs/handlers.py
+++ DazaiRobot/modules/helper_funcs/handlers.py
@@ -8,8 +8,8 @@ from pyrate_limiter import (
 from telegram import Update
 from telegram.ext import CommandHandler, Filters, MessageHandler, RegexHandler
 
-import FallenRobot.modules.sql.blacklistusers_sql as sql
-from FallenRobot import ALLOW_EXCL, DEMONS, DEV_USERS, DRAGONS, TIGERS, WOLVES
+import DazaiRobot.modules.sql.blacklistusers_sql as sql
+from DazaiRobot import ALLOW_EXCL, DEMONS, DEV_USERS, DRAGONS, TIGERS, WOLVES
 
 if ALLOW_EXCL:
     CMD_STARTERS = ("/", "!")
