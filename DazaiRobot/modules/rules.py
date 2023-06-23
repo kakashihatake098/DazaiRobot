e77a349f6d884c2e5cf39532c120e109ee8733e2
adi6804 <120642069+adi6804@users.noreply.github.com>
2023-06-23 23:18:16 +0530

    Update rules.py

--- DazaiRobot/modules/rules.py
+++ DazaiRobot/modules/rules.py
@@ -3,10 +3,10 @@ from telegram.error import BadRequest
 from telegram.ext import CallbackContext, CommandHandler
 from telegram.utils.helpers import escape_markdown
 
-import FallenRobot.modules.sql.rules_sql as sql
-from FallenRobot import dispatcher
-from FallenRobot.modules.helper_funcs.chat_status import connection_status, user_admin
-from FallenRobot.modules.helper_funcs.string_handling import markdown_parser
+import DazaiRobot.modules.sql.rules_sql as sql
+from DazaiRobot import dispatcher
+from DazaiRobot.modules.helper_funcs.chat_status import connection_status, user_admin
+from DazaiRobot.modules.helper_funcs.string_handling import markdown_parser
 
 
 @connection_status
