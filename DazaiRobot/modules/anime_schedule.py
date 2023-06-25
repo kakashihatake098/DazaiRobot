205828bceddb731a92c65b9ef2835db2b2405ccf
Anonymous04 <120642069+adi6804@users.noreply.github.com>
2023-06-25 16:39:21 +0530

    Update anime_schedule.py

--- DazaiRobot/modules/anime_schedule.py
+++ DazaiRobot/modules/anime_schedule.py
@@ -2,33 +2,11 @@
 STATUS: Code is working. ✅
 """
 
-"""
-GNU General Public License v3.0
-
-Copyright (C) 2022, SOME-1HING [https://github.com/SOME-1HING]
-
-Credits:-
-    Ryu120 [https://github.com/Ryu120] 
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <https://www.gnu.org/licenses/>.
-"""
-
 from pyrogram import filters
 import requests
-from Shikimori import pbot
+from DazaiRobot import dispatcher
 
-@pbot.on_message(filters.command('latest'))
+@dispatcher.on_message(filters.command('latest'))
 def schedule(_, message):
     results = requests.get('https://subsplease.org/api/?f=schedule&h=true&tz=Japan').json()
     text = None
