4ec79d83a04d158016c66e7597befff57330f09c
Anonymous04 <120642069+adi6804@users.noreply.github.com>
2023-06-25 20:45:25 +0530

    Update eval.py

--- DazaiRobot/modules/eval.py
+++ DazaiRobot/modules/eval.py
@@ -75,7 +75,7 @@ def do(func, bot, update):
 
     os.chdir(os.getcwd())
     with open(
-        os.path.join(os.getcwd(), "FallenRobot/modules/helper_funcs/temp.txt"), "w"
+        os.path.join(os.getcwd(), "DazaiRobot/modules/helper_funcs/temp.txt"), "w"
     ) as temp:
         temp.write(body)
 
