0040fb6f392112ed7184fdc06549acaab356397a
Anonymous04 <120642069+adi6804@users.noreply.github.com>
2023-06-25 16:36:28 +0530

    Add files via upload

---
+++ DazaiRobot/modules/anime_schedule.py
@@ -0,0 +1,54 @@
+"""
+STATUS: Code is working. ✅
+"""
+
+"""
+GNU General Public License v3.0
+
+Copyright (C) 2022, SOME-1HING [https://github.com/SOME-1HING]
+
+Credits:-
+    Ryu120 [https://github.com/Ryu120] 
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
+
+from pyrogram import filters
+import requests
+from Shikimori import pbot
+
+@pbot.on_message(filters.command('latest'))
+def schedule(_, message):
+    results = requests.get('https://subsplease.org/api/?f=schedule&h=true&tz=Japan').json()
+    text = None
+    for result in results['schedule']:
+        title = result['title']
+        time = result['time']
+        aired = bool(result['aired'])
+        title = f"**[{title}](https://subsplease.org/shows/{result['page']})**" if not aired else f"**~~[{title}](https://subsplease.org/shows/{result['page']})~~**"
+        data = f"{title} - **{time}**"
+        
+        if text:
+            text = f"{text}\n{data}"
+        else:
+            text = data
+
+    message.reply_text(f"**Today's Schedule:**\nTime-Zone: Tokyo (GMT +9)\n\n{text}")
+
+
+__mod_name__ = "𝐒ᴄʜᴇᴅᴜʟᴇ"
+
+__help__ = """
+ ❍ `/latest`: to see latest anime episode
+"""
---
+++ DazaiRobot/modules/ann.py
@@ -0,0 +1,67 @@
+"""
+STATUS: Code is working. ✅
+"""
+
+"""
+GNU General Public License v3.0
+
+Copyright (C) 2022, SOME-1HING [https://github.com/SOME-1HING]
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
+
+# Imports from external libraries. (DON'T EDIT)
+import requests
+from telegram import ParseMode
+from telegram.ext import CommandHandler
+
+# Imports dispatcher = updater.dispatcher from __init__.py (*MUST EDIT* CHANGE MODULE NAME TO THE FOLDER NAME OF MODULES IN YOUR BOT)
+from Shikimori import dispatcher
+
+# Main code, Credit to https://github.com/itspro-dev for making the API. 
+def ann(update, context):
+    try:
+        msg = update.effective_message
+        # API (DON'T EDIT)
+        url = f'https://api.animeepisode.org/waifu/animenews.php'
+        result = requests.get(url).json()
+        img = result['Post_image']
+        # Message (EDIT THIS PART AS HTML *IF YOU WANT*)
+        text = f'''
+<b>Title :</b> <code>{result['Post_title']}</code>
+        
+<b>Description :</b> <code>{result['Description']}</code>
+
+<b>For more info :</b> <code>{result['Post_url']}</code>
+'''
+        msg.reply_photo(photo=img, caption=text, parse_mode=ParseMode.HTML)
+
+    except Exception as e:
+        text = f'<b>Error</b>: <code>' + e + '</code>'
+        msg.reply_text(text, parse_mode=ParseMode.HTML)
+
+# Code Handler (YOU CAN CHANGE 'ann' TO ANY 'cmd' FOR THIS TO BE WORKED AS '/cmd' *IF YOU WANT*.)
+ANN_HANDLER = CommandHandler('ann', ann, run_async=True)
+dispatcher.add_handler(ANN_HANDLER)
+
+#  Buttons for /help .
+__mod_name__ = '𝐀ɴɪᴍᴇ-ɴᴇᴡꜱ-ɴᴇᴛᴡᴏʀᴋ'  # *IF YOU WANT* EDIT NAME OF BUTTON IN '/help'
+
+# *IF YOU WANT* EDIT MESSAGE FOR HELP OF THIS MODULE.
+__help__ = '''
+❍ `/ann` : Gives latest Anime news.
+'''
+
+# DON'T EDIT
+__handlers__ = [ANN_HANDLER]
