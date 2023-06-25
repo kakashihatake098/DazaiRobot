fa43c36a48374bdcc968df6091deb55898e7b9d4
Anonymous04 <120642069+adi6804@users.noreply.github.com>
2023-06-25 16:38:04 +0530

    Update animation.py

--- DazaiRobot/modules/animation.py
+++ DazaiRobot/modules/animation.py
@@ -2,35 +2,13 @@
 STATUS: Code is working. ✅
 """
 
-"""
-GNU General Public License v3.0
-
-Copyright (C) 2022, SOME-1HING [https://github.com/SOME-1HING]
-
-Credits:-
-    I don't know who originally wrote this code. If you originally wrote this code, please reach out to me. 
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <https://www.gnu.org/licenses/>.
-"""
-
 import time
 
 from telegram import Update
 from telegram.ext import CallbackContext
 
-from Shikimori import dispatcher
-from Shikimori.modules.disable import DisableAbleCommandHandler
+from DazaiRobot import dispatcher
+from DazaiRobot.modules.disable import DisableAbleCommandHandler
 
 # sleep how many times after each edit in 'love'
 EDIT_SLEEP = 1
