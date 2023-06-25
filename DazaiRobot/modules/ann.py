df831aed631cc2afaac4436af6a932bfeeac80ff
Anonymous04 <120642069+adi6804@users.noreply.github.com>
2023-06-25 16:40:17 +0530

    Update ann.py

--- DazaiRobot/modules/ann.py
+++ DazaiRobot/modules/ann.py
@@ -2,32 +2,13 @@
 STATUS: Code is working. ✅
 """
 
-"""
-GNU General Public License v3.0
-
-Copyright (C) 2022, SOME-1HING [https://github.com/SOME-1HING]
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <https://www.gnu.org/licenses/>.
-"""
-
 # Imports from external libraries. (DON'T EDIT)
 import requests
 from telegram import ParseMode
 from telegram.ext import CommandHandler
 
 # Imports dispatcher = updater.dispatcher from __init__.py (*MUST EDIT* CHANGE MODULE NAME TO THE FOLDER NAME OF MODULES IN YOUR BOT)
-from Shikimori import dispatcher
+from DazaiRobot import dispatcher
 
 # Main code, Credit to https://github.com/itspro-dev for making the API. 
 def ann(update, context):
