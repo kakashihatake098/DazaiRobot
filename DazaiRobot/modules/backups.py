c3295847dc588762bd481590facfabdb9e1c63a4
Anonymous04 <120642069+adi6804@users.noreply.github.com>
2023-06-25 20:51:08 +0530

    Update backups.py

--- DazaiRobot/modules/backups.py
+++ DazaiRobot/modules/backups.py
@@ -323,7 +323,7 @@ def export_data(update, context):
         },
     }
     baccinfo = json.dumps(backup, indent=4)
-    with open("FallenRobot{}.backup".format(chat_id), "w") as f:
+    with open("DazaiRobot{}.backup".format(chat_id), "w") as f:
         f.write(str(baccinfo))
     context.bot.sendChatAction(current_chat_id, "upload_document")
     tgl = time.strftime("%H:%M:%S - %d/%m/%Y", time.localtime(time.time()))
@@ -339,7 +339,7 @@ def export_data(update, context):
         pass
     context.bot.sendDocument(
         current_chat_id,
-        document=open("FallenRobot{}.backup".format(chat_id), "rb"),
+        document=open("DazaiRobot{}.backup".format(chat_id), "rb"),
         caption="📤*Successfully Exported backup:*\nChat: `{}`\nChat ID: `{}`\nOn: `{}`\n\nNote: This `FallenRobot-Backup` was specially made for notes 📚.".format(
             chat.title, chat_id, tgl
         ),
@@ -347,7 +347,7 @@ def export_data(update, context):
         reply_to_message_id=msg.message_id,
         parse_mode=ParseMode.MARKDOWN,
     )
-    os.remove("FallenRobot{}.backup".format(chat_id))  # Cleaning file
+    os.remove("DazaiRobot{}.backup".format(chat_id))  # Cleaning file
 
 
 # Temporary data
