59349fcff22b456da8cc3ce3d4eba3c6ca377219
Anonymous04 <120642069+adi6804@users.noreply.github.com>
2023-06-25 17:21:05 +0530

    Update waifu.py

--- DazaiRobot/modules/waifu.py
+++ DazaiRobot/modules/waifu.py
@@ -2,25 +2,6 @@
 STATUS: Code is working. ✅
 """
 
-"""
-GNU General Public License v3.0
-
-Copyright (C) 2022, SOME-1HING [https://github.com/SOME-1HING]
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <https://www.gnu.org/licenses/>.
-"""
-
 # Created By: https://telegram.dog/SOME1_HING 
 # Assisted By: https://telegram.dog/SastaDev from https://telegram.dog/SastaNetwork
 
@@ -31,7 +12,7 @@ from telegram.ext import CommandHandler
 
 import random
 # Imports dispatcher = updater.dispatcher from __init__.py (*MUST EDIT* CHANGE MODULE NAME TO THE FOLDER NAME OF MODULES IN YOUR BOT)
-from Shikimori import dispatcher
+from DazaiRobot import dispatcher
 
 # Main code, Credit to https://github.com/itspro-dev for making the API. 
 
@@ -60,7 +41,7 @@ WAIFUINFO_HANDLER = CommandHandler('waifuinfo', waifu, run_async=True)
 dispatcher.add_handler(WAIFUINFO_HANDLER)
 
 #  Buttons for /help .
-__mod_name__ = '𝐖ᴀɪꜰᴜꜱ'  # *IF YOU WANT* EDIT NAME OF BUTTON IN '/help'
+__mod_name__ = 'ᴡᴀɪғᴜs'  # *IF YOU WANT* EDIT NAME OF BUTTON IN '/help'
 
 
 # *IF YOU WANT* EDIT MESSAGE FOR HELP OF THIS MODULE.
