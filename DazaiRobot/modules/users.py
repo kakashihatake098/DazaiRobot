cc988ddce6f535a0474d8c8e6a949f22b254aaf7
Anonymous04 <adi855053@gmail.com>
2023-06-29 09:45:38 +0530

    Update users.py

--- DazaiRobot/modules/users.py
+++ DazaiRobot/modules/users.py
@@ -5,7 +5,7 @@ from pyrogram.types import Message
 from telegram import TelegramError, Update
 from telegram.error import BadRequest, Unauthorized
 from telegram.ext import CallbackContext, CommandHandler, Filters, MessageHandler
-import DazaiRobot.modules.no_sql.users_db as user_db 
+import DazaiRobot.modules.sql.users_sql as user_db 
 from DazaiRobot import pbot as app
 from DazaiRobot import DEV_USERS, LOGGER, OWNER_ID, dispatcher
 from DazaiRobot.modules.helper_funcs.chat_status import dev_plus, sudo_plus
